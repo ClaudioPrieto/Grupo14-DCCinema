d7a85d88c0ba80fb091746a4c0deeac20192dfa3
Joaquín <jncaceres@uc.cl>
2021-11-18 04:06:42 -0300

    deleted github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,31 +16,6 @@
 require 'capybara/rspec'
 require 'rails_helper'
 require 'database_cleaner/active_record'
-require "selenium/webdriver"
-require "webdrivers/chromedriver"
-
-Webdrivers::Chromedriver.required_version = "74.0.3729.6"
-
-Capybara.server = :puma, { Silent: true }
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w(no-sandbox headless disable-gpu window-size=1280,800),
-    },
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-end
-
-Capybara.javascript_driver = :headless_chrome
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
