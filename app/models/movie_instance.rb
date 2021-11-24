0957526f0b59225e3f4d8e9022bb1f7f13c33f68
ClaudioPrieto <cjprieto@uc.cl>
2021-11-23 23:22:34 -0300

    added instance description and route button

--- app/models/movie_instance.rb
+++ app/models/movie_instance.rb
@@ -2,4 +2,8 @@ class MovieInstance < ApplicationRecord
   belongs_to :movie
   belongs_to :room
   has_many :reservations
+
+  def movie_info
+    "Sala #{self.room_id} #{schedule}: #{self.movie.name}"
+  end
 end
