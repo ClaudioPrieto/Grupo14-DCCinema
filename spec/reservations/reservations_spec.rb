6239fd36c2cab82d28e2830994f75c21b1e71acb
Joaquín <jncaceres@uc.cl>
2021-11-27 23:30:26 -0300

    arregloo en tests

--- spec/reservations/reservations_spec.rb
+++ spec/reservations/reservations_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Reservations', type: :system do
       visit 'movie_instances/new'
       select "Shingeki no Kyojin", :from => "movie_instance_movie_id"
       select 1 , :from => "movie_instance_room_id"
-      select "2021", :from => "movie_instance_day_1i"
+      select "2022", :from => "movie_instance_day_1i"
       select "November", :from => "movie_instance_day_2i"
       select "23", :from => "movie_instance_day_3i"
       select "MATINE", :from => "movie_instance_schedule"
@@ -22,7 +22,7 @@ RSpec.describe 'Reservations', type: :system do
 
   it 'create reservation' do
       visit '/movie_instances'
-      click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+      click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
       fill_in 'reservation_username', :with => 'Tester'
       select 'A', :from => "row-selector"
       select 1, :from => "col-selector"
@@ -33,25 +33,25 @@ RSpec.describe 'Reservations', type: :system do
 
   it 'see info of reservation' do
       visit '/movie_instances'
-      click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+      click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
       fill_in 'reservation_username', :with => 'Tester'
       select 'A', :from => "row-selector"
       select 1, :from => "col-selector"
       click_button 'Reservar'
 
-      click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+      click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
       expect(page).to have_select('col-selector', :options => ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"])
   end
 
   it 'see view of reservation css' do
     visit '/movie_instances'
-    click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+    click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
     fill_in 'reservation_username', :with => 'Tester'
     select 'A', :from => "row-selector"
     select 1, :from => "col-selector"
     click_button 'Reservar'
 
-    click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+    click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
     expect(page).to have_css(".row", :count => 5)
     expect(page).to have_css(".bg-success", :count => 12*4 - 1)
     expect(page).to have_css(".bg-danger", :count => 1)
@@ -60,7 +60,7 @@ end
 
   it 'return to instances' do
       visit '/movie_instances'
-      click_link 'Shingeki no Kyojin_2021-11-23_new_reservation'
+      click_link 'Shingeki no Kyojin_2022-11-23_new_reservation'
       click_link 'Volver'
       expect(page).to have_content('Funciones')
   end
